709acb6993ab7588ec13460b68b8ec8baec421ac
Diggaj Ugvekar <diggajugvekar@gmail.com>
2023-07-23 21:33:26 +0530

    Shaping Spectrum

--- script.js
+++ script.js
@@ -16,13 +16,14 @@ function draw(){   // used to update the canvas and handle animations
     stroke(255)
 
     var wave = fft.waveform()
-
+    beginShape()
     for(var i = 0; i < width; i++){
         var index = floor(map(i, 0, width, 0, wave.length))
         var x = i
         var y = wave[index] * 300 + height / 2
-        point(x,y)
+        vertex(x,y)
     }
+    endShape()
 }
 
 
